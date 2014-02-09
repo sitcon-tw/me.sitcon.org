4b4996b080228e1d62eed905203df2b3d2767eff
蒼時弦也 <elct9620@frost.tw>
2014-02-09 18:38:09 +0800

    Fix# fix 404 not found handler didn't support .erb extension

--- http_servlet_handler.rb
+++ http_servlet_handler.rb
@@ -5,11 +5,14 @@ class NotFoundHandler
   end
 
   def call(env)
-    unless File.exists?(env["PATH_INFO"][1..-1])
-      env["PATH_INFO"] = @file
-    end
+    env["PATH_INFO"] = @file unless is_file_exists?(env)
     status, headers, content = @app.call(env)
     [status, headers, content]
+  end
+
+  def is_file_exists?(env)
+    path = env["PATH_INFO"][1..-1]
+    File.exist?(path) || File.exists?("#{path}.erb")
   end
 end
 
